51412a2d2640548e0316297d0b9e3106eecc563c
Malhar Jadhav <malharjadhav@gmail.com>
2022-02-15 23:18:25 +0530

    modifications - models, views, routes, controllers (made api's in another app)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sign_up, only: [:index]
+
+  get "/sign_in", to: "sign_in#index"  
+  get "/", to: "main#index"
+
 end
